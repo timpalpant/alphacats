1510fb8b79b37743330218b08272808d418f2651
Timothy Palpant <tim@palpant.us>
2019-02-24 13:46:09 -0500

    rename

--- cmd/alphacats/main.go
+++ cmd/alphacats/main.go
@@ -17,8 +17,9 @@ import (
 func main() {
 	seed := flag.Int64("seed", 123, "Random seed")
 	iter := flag.Int("iter", 100, "Number of DeepCFR iterations to perform")
-	bufSize := flag.Int("buf_size", 1000000, "Size of reservoir sample buffer")
-	traversalsPerIter := flag.Int("traversals_per_iter", 10, "Number of ES-CFR traversals to perform each iteration")
+	bufSize := flag.Int("buf_size", 10000000, "Size of reservoir sample buffer")
+	traversalsPerIter := flag.Int("traversals_per_iter", 10,
+		"Number of ES-CFR traversals to perform each iteration")
 	flag.Parse()
 
 	rand.Seed(*seed)
@@ -27,22 +28,20 @@ func main() {
 	lstm := model.NewLSTM(model.DefaultParams())
 	buffer := deepcfr.NewReservoirBuffer(*bufSize)
 	deepCFR := deepcfr.New(lstm, buffer)
-	opt := cfr.New(cfr.Params{
+	opt := cfr.New(cfr.SamplingParams{
 		SampleChanceNodes:     true,
 		SampleOpponentActions: true,
-		LinearWeighting:       true,
 	}, deepCFR)
 
 	for t := 1; t <= *iter; t++ {
-		glog.Infof("Starting iter t=%v", t)
-
-		glog.Infof("Collecting samples")
+		glog.Infof("[t=%d] Collecting samples", t)
 		for k := 1; k <= *traversalsPerIter; k++ {
+			glog.Infof("[k=%d] Running ES-CFR on random game", k)
 			game := alphacats.NewRandomGame()
 			opt.Run(game)
 		}
 
-		glog.Infof("Training network")
+		glog.Infof("[t=%d]  Training network", t)
 		deepCFR.Update()
 	}
 }
